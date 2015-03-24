58f370d47308fdee15da67c7422dceffe6ab3b88
Sébastien Santoro <dereckson@espace-win.org>
2015-03-24 18:20:29 +0000

    Updated docker run command for Nasqueron
    Per Nasqueron Docker deployment squad guidelines,
    application-specific files should be in /data.
    
    Image tagname updated, as we'll use the version in our repo.

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-mkdir -p /srv/var/log/sympa
-mkdir -p /srv/sympa/etc/includes
-mkdir -p /srv/sympa/etc/shared
-mkdir -p /srv/sympa/spool
-mkdir -p /srv/sympa/nullmailer
-mkdir -p /srv/sympa/data
+mkdir -p /data/sympa/log
+mkdir -p /data/sympa/etc/includes
+mkdir -p /data/sympa/etc/shared
+mkdir -p /data/sympa/spool
+mkdir -p /data/sympa/nullmailer
+mkdir -p /data/sympa/data
 
-# /srv/sympa/etc/includes/database contains a password for sympa user with
+# /data/sympa/etc/includes/database contains a password for sympa user with
 # permissions over sympa PostgreSQL database at pgsql.server2.docker
-# /srv/sympa/etc/includes/cookie contains crypto cookie string secret
+# /data/sympa/etc/includes/cookie contains crypto cookie string secret
 
 # You can create the database with:
 # createuser -h pgsql.server2.docker -PE -DRS -U postgres -W sympa
@@ -18,4 +18,4 @@ mkdir -p /srv/sympa/data
 # You might have to initialize the database with:
 # psql -h pgsql.server2.docker -U sympa -W -f /usr/share/sympa/bin/create_db.Pg
 
-docker run -d --restart=always --name sympa --hostname sympa -e SET_REAL_IP_FROM=172.17.0.0/16 -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/sympa:/var/log/sympa -v /srv/sympa/etc/includes:/etc/sympa/includes -v /srv/sympa/etc/shared:/etc/sympa/shared -v /srv/sympa/spool:/var/spool/sympa -v /srv/sympa/nullmailer:/var/spool/nullmailer -v /srv/sympa/data:/var/lib/sympa cloyne/sympa
+docker run -d --restart=always --name sympa --hostname sympa -e SET_REAL_IP_FROM=172.17.0.0/16 -v /data/var/hosts:/etc/hosts:ro -v /data/sympa/log:/var/log/sympa -v /data/sympa/etc/includes:/etc/sympa/includes -v /data/sympa/etc/shared:/etc/sympa/shared -v /data/sympa/spool:/var/spool/sympa -v /data/sympa/nullmailer:/var/spool/nullmailer -v /data/sympa/data:/var/lib/sympa nasqueron/sympa
